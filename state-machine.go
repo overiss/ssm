51ef7cda566a4d79bdf14c5c7416b2e8e0731cc1
overiss <ovveriss@gmail.com>
2026-04-15 18:57:55 +0300

    remove spaces

--- state-machine.go
+++ state-machine.go
@@ -16,8 +16,7 @@ func (b *boiler_plate) ApplyCfg(cfg *Config) *boiler_plate { b.cfg = cfg; return
 // AddState - adds an executable state to the machine. You can specify a custom
 // state name as the second argument, otherwise it will be determined by default.
 func (b *boiler_plate) AddState(s State, state_name string) *boiler_plate {
-	b.states, b.names = append(b.states, s), append(b.names, state_name)
-	b.state_map[state_name] = len(b.states) - 1
+	b.states, b.names = append(b.states, s), append(b.names, state_name); b.state_map[state_name] = len(b.states) - 1
 	return b
 }
 
@@ -26,12 +25,7 @@ func (b *boiler_plate) Build() *Machine { return &Machine{bp: b} }
 
 func (m *Machine) Run() {
 	for {
-		select {
-		case <-m.bp.ctx.Done():
-			return
-		default:
-			m.exec()
-		}
+		select {case <-m.bp.ctx.Done(): return; default: m.exec()}
 	}
 }
 
@@ -46,42 +40,26 @@ func new_caller() *Caller {
 
 func (m *Machine) exec() {
 	m.loop_sleep_sync()
-	if _sh := m.bp.cfg.Start_handler; _sh != nil {
-		_sh(m.bp.names[m.bp.last_state])
-	}
+	if _sh := m.bp.cfg.Start_handler; _sh != nil {_sh(m.bp.names[m.bp.last_state])}
 	crucial, clr, exec_chan := m.bp.states[m.bp.last_state], new_caller(), make(chan error, 1)
 	go func() { defer close(exec_chan); exec_chan <- crucial(clr) }()
 
 	select {
-	case e := <-exec_chan:
-		if _eh := m.bp.cfg.Err_handler; e != nil && _eh != nil {
-			_eh(e)
-			return
-		}
-	case <-clr.continue_sig:
-		return
+	case e := <-exec_chan: if _eh := m.bp.cfg.Err_handler; e != nil && _eh != nil {_eh(e);return}
+	case <-clr.continue_sig: return
 	case t := <-clr.change_sig:
 		i, ok := m.bp.state_map[t]
-		if !ok {
-			print("[SSM] state not found:", t)
-		}
-		m.bp.last_state = i
-		return
+		if !ok {print("[SSM] state not found:", t)}; m.bp.last_state = i; return
 	}
 	m.next_step()
 }
 
 func (m *Machine) loop_sleep_sync() {
 	last_loop := m.bp.last_state+1 == len(m.bp.states)
-	if loop_to := m.bp.cfg.Loop_tm; loop_to == 0 && last_loop {
-		time.Sleep(loop_to)
-	}
+	if loop_to := m.bp.cfg.Loop_tm; loop_to == 0 && last_loop {time.Sleep(loop_to)}
 }
 
 func (m *Machine) next_step() {
-	if m.bp.last_state+1 == len(m.bp.states) {
-		m.bp.last_state = 0
-		return
-	}
+	if m.bp.last_state+1 == len(m.bp.states) {m.bp.last_state = 0; return}
 	m.bp.last_state += 1
 }
